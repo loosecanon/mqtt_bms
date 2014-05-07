5d28e884d522a4d9e3a10828075eeb045b83b681
Ben Canon <bencanon@gmail.com>
2014-05-07 12:16:15 -0400

    Used Twisted to schedule message every 1m on BBB

--- bbb.py
+++ bbb.py
@@ -2,21 +2,25 @@ from twisted.internet import reactor, task
 import mosquitto
 from datetime import datetime
 
-def runEverySecond():
-
-
-client = mosquitto.Mosquitto("bfc_beaglebone")
-rc = client.connect("ec2-54-201-108-65.us-west-2.compute.amazonaws.com", port = 1883, keepalive = 60)
 messages = 0
-last_min = datetime.now().minute
-while rc == 0:
-	now = datetime.now().minute
-	if(now > last_min):
-		print("sent message: " + str(messages))
-		now = datetime.now()
-		now = now.replace(microsecond = 0)
-		rc = client.publish("bfcanon_test/8111ANNA/OFFICE/TEMP", "BBB_TEMP" + "~" + str(now) + "~42", 1)
-		last_min = datetime.now().minute
-	rc = client.loop()
-	messages += 1
-	
+client = mosquitto.Mosquitto("bfc_beaglebone")
+l = task.LoopingCall(runEveryMinute)
+rc = 0
+
+def __main__():
+    rc = client.connect("ec2-54-201-108-65.us-west-2.compute.amazonaws.com", port = 1883, keepalive = 60)
+    l = task.LoopingCall(runEveryMinute)
+    l.start(60.0)
+
+def runEveryMinute():
+    now = datetime.now()
+    now = now.replace(microsecond = 0)
+    print("sent message: " + str(messages))
+    messages = messages + 1
+    rc = client.publish("bfcanon_test/8111ANNA/OFFICE/TEMP", "BBB_TEMP" + "~" + str(now) + "~42", 1)
+    if rc != 0:
+        print("RC != 0, rc = " + str(rc))
+        l.stop()
+
+
+
