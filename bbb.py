415afd6a9dde0549d52aa3bccc12ea159b7f6b96
Ben Canon <admin@loosecanon.net>
2014-05-07 13:49:54 -0400

    Used Twisted to schedule message every 1m on BBB

--- bbb.py
+++ bbb.py
@@ -4,13 +4,15 @@ from datetime import datetime
 
 messages = 0
 client = mosquitto.Mosquitto("bfc_beaglebone")
-l = task.LoopingCall()
+
 rc = 0
 
 def __main__():
     rc = client.connect("ec2-54-201-108-65.us-west-2.compute.amazonaws.com", port = 1883, keepalive = 60)
     l = task.LoopingCall(runEveryMinute)
     l.start(60.0)
+    reactor.run()
+
 
 def runEveryMinute():
     now = datetime.now()
@@ -18,9 +20,7 @@ def runEveryMinute():
     print("sent message: " + str(messages))
     messages = messages + 1
     rc = client.publish("bfcanon_test/8111ANNA/OFFICE/TEMP", "BBB_TEMP" + "~" + str(now) + "~42", 1)
-    if rc != 0:
-        print("RC != 0, rc = " + str(rc))
-        l.stop()
+
 
 
 
